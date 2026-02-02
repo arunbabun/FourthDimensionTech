831c939a808e25e6452d16b55a246578b7528d98
Arun Babu Narayanaswamy <90788299+arunbabun@users.noreply.github.com>
2026-02-01 19:57:23 -0500

    commit

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                       <img 
                         src={heroBannerBg} 
                         alt="" 
-                        className="w-full h-full object-cover" 
+                        className="w-full h-full object-cover opacity-60" 
                       />
                     </div>
                     
